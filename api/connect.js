4d1fd710db946c70fd487bda5d4334b0479f8445
marwan <marwan.h1994@gmail.com>
2023-08-15 00:17:00 +0200

    added env files

--- api/connect.js
+++ api/connect.js
@@ -1,9 +1,11 @@
+import dotenv from "dotenv"
 import mysql from "mysql"
 
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "hyfuser",
-    password:"hyfpassword",
-    database: "social"
+dotenv.config()
 
+export const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 })
--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
+const apiUrl = `${process.env.REACT_APP_API_BASE_URL}/api` 
+
 export const makeRequest = axios.create({
-   baseURL: "http://localhost:3006/api/",
-   withCredentials:true, 
+    baseURL: apiUrl,
+    withCredentials: true,
 })
