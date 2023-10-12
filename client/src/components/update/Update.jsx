edc2b5d32dab7d1cc22b16c94e3bcb7e91269dbc
marwan <marwan.h1994@gmail.com>
2023-10-12 23:57:58 +0200

    fix same style issues and add elect options for update user

--- client/src/components/update/Update.jsx
+++ client/src/components/update/Update.jsx
@@ -5,7 +5,69 @@ import { useMutation, useQueryClient } from "@tanstack/react-query"
 import IconButton from "@mui/material/IconButton"
 import PhotoCameraIcon from "@mui/icons-material/PhotoCamera"
 
-const Update = ({ setOpenUpdate, user}) => {
+const mostUsedOptions = {
+    work: [
+        "Software Engineer",
+        "Data Analyst",
+        "Graphic Designer",
+        "Nurse",
+        "Teacher",
+        "Marketing Manager",
+        "Accountant",
+        "Sales Representative",
+        "Chef",
+        "Other",
+    ],
+    school: [
+        "School",
+        "University",
+        "Technical College",
+        "Online Courses",
+        "Other",
+    ],
+    hobbies: [
+        "Reading",
+        "Hiking",
+        "Cooking",
+        "Photography",
+        "Traveling",
+        "Painting",
+        "Gardening",
+        "Sports",
+        "Music",
+        "Other",
+    ],
+    languages: [
+        "English",
+        "Arabic",
+        "Spanish",
+        "French",
+        "German",
+        "Chinese",
+        "Japanese",
+        "Russian",
+        "Arabic",
+        "Portuguese",
+        "Other",
+    ],
+    nationality: [
+        "Arab",
+        "American",
+        "British",
+        "Canadian",
+        "Indian",
+        "Australian",
+        "German",
+        "French",
+        "Japanese",
+        "Chinese",
+        "Dutch",
+        "Other",
+    ],
+    age: Array.from({ length: 93 }, (_, i) => i + 18),
+}
+
+const Update = ({ setOpenUpdate, user }) => {
     const [cover, setCover] = useState(null)
     const [profile, setProfile] = useState(null)
     const [texts, setTexts] = useState({
@@ -13,7 +75,7 @@ const Update = ({ setOpenUpdate, user}) => {
         city: user.city || "",
         school: user.school || "",
         work: user.work || "",
-        languages: user.languages_spoken || "", 
+        languages_spoken: user.languages_spoken || "",
         hobbies: user.hobbies || "",
         nationality: user.nationality || "",
         age: user.age || null,
@@ -29,10 +91,10 @@ const Update = ({ setOpenUpdate, user}) => {
             console.log(err)
         }
     }
-const handleChange = (e) => {
-    const { name, value } = e.target
-    setTexts((prev) => ({ ...prev, [name]: value }))
-}
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setTexts((prev) => ({ ...prev, [name]: value }))
+    }
 
     const queryClient = useQueryClient()
 
@@ -46,7 +108,6 @@ const handleChange = (e) => {
             },
         }
     )
-   
 
     const handleClick = async (e) => {
         e.preventDefault()
@@ -66,42 +127,46 @@ const handleChange = (e) => {
     return (
         <div className="update">
             <form>
-                {/* ... (existing labels and inputs) */}
-                <label htmlFor="cover">
-                    Cover Image:
-                    <input
-                        id="cover"
-                        type="file"
-                        onChange={(e) => setCover(e.target.files[0])}
-                        style={{ display: "none" }} // hide the default file input
-                    />
-                    <IconButton
-                        component="span"
-                        color="primary"
-                        aria-label="upload cover picture"
-                        style={{ marginLeft: "10px" }}
-                    >
-                        <PhotoCameraIcon />
-                    </IconButton>
-                </label>
-
-                <label htmlFor="profile">
-                    Profile Image:
-                    <input
-                        id="profile"
-                        type="file"
-                        onChange={(e) => setProfile(e.target.files[0])}
-                        style={{ display: "none" }} // hide the default file input
-                    />
-                    <IconButton
-                        component="span"
-                        color="primary"
-                        aria-label="upload profile picture"
-                        style={{ marginLeft: "10px" }}
-                    >
-                        <PhotoCameraIcon />
-                    </IconButton>
-                </label>
+                <div className="images-container">
+                    <div className="image-upload">
+                        <label htmlFor="cover">
+                            Cover Image:
+                            <input
+                                id="cover"
+                                type="file"
+                                onChange={(e) => setCover(e.target.files[0])}
+                                style={{ display: "none" }}
+                            />
+                            <IconButton
+                                component="span"
+                                color="primary"
+                                aria-label="upload cover picture"
+                                style={{ marginLeft: "10px" }}
+                            >
+                                <PhotoCameraIcon />
+                            </IconButton>
+                        </label>
+                    </div>
+                    <div className="image-upload">
+                        <label htmlFor="profile">
+                            Profile Image:
+                            <input
+                                id="profile"
+                                type="file"
+                                onChange={(e) => setProfile(e.target.files[0])}
+                                style={{ display: "none" }}
+                            />
+                            <IconButton
+                                component="span"
+                                color="primary"
+                                aria-label="upload profile picture"
+                                style={{ marginLeft: "10px" }}
+                            >
+                                <PhotoCameraIcon />
+                            </IconButton>
+                        </label>
+                    </div>
+                </div>
 
                 <label htmlFor="name">Name:</label>
                 <input
@@ -122,55 +187,83 @@ const handleChange = (e) => {
                 />
 
                 <label htmlFor="school">School:</label>
-                <input
+                <select
                     id="school"
-                    type="text"
                     name="school"
                     value={texts.school}
                     onChange={handleChange}
-                />
+                >
+                    {mostUsedOptions.school.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
                 <label htmlFor="work">Work:</label>
-                <input
+                <select
                     id="work"
-                    type="text"
                     name="work"
                     value={texts.work}
                     onChange={handleChange}
-                />
+                >
+                    {mostUsedOptions.work.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
                 <label htmlFor="languages_spoken">Languages:</label>
-                <input
+                <select
                     id="languages_spoken"
-                    type="text"
                     name="languages_spoken"
-                    placeholder={user.languages_spoken}
+                    value={texts.languages_spoken}
                     onChange={handleChange}
-                />
-
+                >
+                    {mostUsedOptions.languages.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
                 <label htmlFor="hobbies">Hobbies:</label>
-                <input
+                <select
                     id="hobbies"
-                    type="text"
-                    name="hobbies" 
-                    placeholder={user.hobbies}
+                    name="hobbies"
+                    value={texts.hobbies}
                     onChange={handleChange}
-                />
-
+                >
+                    {mostUsedOptions.hobbies.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
                 <label htmlFor="nationality">Nationality:</label>
-                <input
+                <select
                     id="nationality"
-                    type="text"
                     name="nationality"
-                    placeholder={user.nationality}
+                    value={texts.nationality}
                     onChange={handleChange}
-                />
+                >
+                    {mostUsedOptions.nationality.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
                 <label htmlFor="age">Age:</label>
-                <input
+                <select
                     id="age"
-                    type="number"
                     name="age"
-                    placeholder={user.age}
+                    value={texts.age}
                     onChange={handleChange}
-                />
+                >
+                    {mostUsedOptions.age.map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
 
                 <button onClick={handleClick}>Update</button>
                 <button onClick={() => setOpenUpdate(false)}>Cancel</button>
