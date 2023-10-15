3fc04c998119ab5d4b1e018e689904f6e0257522
marwan <marwan.h1994@gmail.com>
2023-10-15 17:00:34 +0200

    fix an issue

--- api/connect.js
+++ api/connect.js
@@ -8,5 +8,4 @@ export const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
 })
