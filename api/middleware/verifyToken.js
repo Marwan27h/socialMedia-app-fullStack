27fc24344c4bfd8be41cc9d66e35eb8351763993
marwan <marwan.h1994@gmail.com>
2023-10-18 02:07:14 +0200

    update verifyToken

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,11 +1,19 @@
 import jwt from "jsonwebtoken"
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.accessToken
-    if (!token) return res.status(401).json("Not authenticated")
+    const token = req.headers.authorization
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
-        if (err) return res.status(403).json("Token is not valid")
+    if (!token) {
+        return res.status(401).json({ message: "No token provided" })
+    }
+
+    // Split the "Bearer" prefix from the token value
+    const tokenValue = token.split(" ")[1]
+
+    jwt.verify(tokenValue, process.env.JWT_SECRET, (err, userInfo) => {
+        if (err) {
+            return res.status(403).json("Token is not valid")
+        }
 
         req.userInfo = userInfo
         next()
